fc685818785c56566440d6d70abcfac913022589
Nir Oren <jhudsy@gmail.com>
2024-12-26 20:39:28 +0000

    updated to work. saving binary in ipynb

--- main.py
+++ main.py
@@ -104,8 +104,11 @@ TRAININGDIR = 'data/training/'
 VALIDATIONDIR = 'data/validation/'
 
 class GameSequence(keras.utils.Sequence):
-    def __init__(self, pgn_dir, batch_size=32, shuffle=True):
-        super().__init__()
+
+    def __init__(self, pgn_dir, batch_size=32, shuffle=True,**kwargs):
+        self.cache = {}
+        
+        super().__init__(**kwargs)
         #self.pgn_files should be the full path to all files in pgn_dir
         self.pgn_files=[pgn_dir+f for f in os.listdir(pgn_dir) if os.path.isfile(os.path.join(pgn_dir, f))]
         
@@ -119,24 +122,28 @@ class GameSequence(keras.utils.Sequence):
         return len(self.pgn_files) // self.batch_size
     
     def __getitem__(self, idx):
+        
         batch_files = self.pgn_files[idx * self.batch_size:(idx + 1) * self.batch_size]
         X, y1,y2 = [], [], []
         for file in batch_files:
-            with open(file) as f:
-                pgn = chess.pgn.read_game(f)
-                X.append(get_game_tensors(pgn,40))
-                #y1.append(rating_to_output(get_ratings(pgn)[0])) #use if using one-hot encoding
-                #y2.append(rating_to_output(get_ratings(pgn)[1])) #use if using one-hot encoding
-                y1.append(get_ratings(pgn)[0])
-                y2.append(get_ratings(pgn)[1])
-                         
+            if file not in self.cache:
+                with open(file) as f:
+                    pgn = chess.pgn.read_game(f)
 
+                    self.cache[file] = get_game_tensors(pgn,40), (get_ratings(pgn)[0],get_ratings(pgn)[1])
+                    #self.cache[file] = get_game_tensors(pgn,40), (rating_to_output(get_ratings(pgn)[0]),rating_to_output(get_ratings(pgn)[1])) #use if using one-hot encoding
+
+            
+            X.append(self.cache[file][0])
+            y1.append(self.cache[file][1][0])
+            y2.append(self.cache[file][1][1])
+                         
         X = np.array(X)
         y1 = np.array(y1)
         y2 = np.array(y2)
 
         #shape is (32, 40, 134) (32, 2, 48) for X and y respectively with a batch size of 32
-
+        
         return X, (y1,y2)
     
     def on_epoch_end(self):
@@ -146,7 +153,7 @@ class GameSequence(keras.utils.Sequence):
 
 ########################################
 #we will have a batch of 32 games at a time for training
-batch_size = 32
+batch_size = 16
 
 #we will train for 100 epochs or when validation loss starts to increase
 epochs = 100
@@ -154,9 +161,9 @@ epochs = 100
 #our input is a 40x66 matrix. We will use 40 time steps and 66 features. The first 64 features are an 8x8 image and the last two features are a (0,1) and a float. The image itself is a chessboard and consists of elements taken from the categorical set {0..12} where 0 is an empty square and 1..12 are the 6 types of pieces for each player. The (0,1) feature is a flag that indicates which player is to move next and the float is the value of the position as evaluated by Stockfish.
 
 input = Input(shape=(40, 134))
-x = LSTM(128, return_sequences=True)(input)
-#x = LSTM(128)(x)
-x = Dense(64, activation='relu')(x)
+x = LSTM(256, return_sequences=True)(input)
+x = LSTM(128)(x)
+x = Dense(128, activation='relu')(x)
 x = Dense(64, activation='relu')(x)
 
 #The output is a pair of 2 vectors of length 48, using a one-hot encoding. Each vector is the player's rating. The first vector is for white, and the seocnd is for black. The ratings are in the range 0..47, where 0 is the lowest rating and 47 is the highest rating. The ratings are integers and are distributed uniformly in the range 0..47.
@@ -179,9 +186,11 @@ model.compile(optimizer='adam',loss = 'mse', metrics=['mse','mse'])
 
 print("output shape:",model.output_shape)
 
-model.fit(GameSequence(TRAININGDIR, batch_size=batch_size),
+model.fit(GameSequence(TRAININGDIR, batch_size=batch_size,max_queue_size=128,workers=10,shuffle=True),
            epochs=epochs,
-           callbacks=[tf.keras.callbacks.ModelCheckpoint('model.keras', save_best_only=True)],
+           callbacks=[tf.keras.callbacks.ModelCheckpoint('model.keras', save_best_only=True,mode='auto',monitor='val_loss'),
+                      tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='auto')],
+           validation_data=GameSequence(VALIDATIONDIR, batch_size=batch_size,max_queue_size=128,workers=10,shuffle=False),
            verbose=1)
 
 model.save('model.keras')

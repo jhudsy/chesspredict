d0eeb47a803bdf2d7b1d3c373f1fd6d50ad44c00
Nir Oren <jhudsy@gmail.com>
2025-01-06 10:00:25 +0000

    trying semaphores

--- ml/data_prep.py
+++ ml/data_prep.py
@@ -7,7 +7,7 @@ from io import TextIOWrapper
 import os
 import argparse
 import concurrent.futures
-import asyncio
+import threading
 
 ####################################################
 
@@ -98,7 +98,7 @@ def _write_to_file(gt1,gt2,white_rating,black_rating,file,file_index):
 
 ####################################################
 
-def _write_callback(future,files,file_indexes,semaphore):
+def _write_callback(future,files,file_indexes,semaphore_list):
     """callback function for the executor. This function writes the game tensor to the file. The future object contains the game tensor. If the game tensor is None, we return.
     Otherwise, we write the game tensor to the file. The file_indexes dictionary is updated to reflect the number of games written to the file."""
     game_tensor = future.result()
@@ -107,7 +107,7 @@ def _write_callback(future,files,file_indexes,semaphore):
     gt1,gt2,white_rating,black_rating,file_name = game_tensor
     f = files[file_name]
     file_indexes[file_name] = _write_to_file(gt1,gt2,white_rating,black_rating,f,file_indexes[file_name])
-    semaphore.release()
+    semaphore_list[0] += 1
 
 ####################################################
 def write_to_hdf5_parallel(reader,path=""):
@@ -128,7 +128,7 @@ def write_to_hdf5_parallel(reader,path=""):
 
     game = ""
     count = 0
-    semaphore = asyncio.Semaphore(1000) #limit the number of concurrent writes to the file
+    semaphore = [1000] #limit the number of concurrent writes to the file
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for line in reader:
@@ -147,7 +147,9 @@ def write_to_hdf5_parallel(reader,path=""):
                         print(f"file {f} has {file_indexes[f]} games and {num_gt} total games")
                 count += 1
 
-                semaphore.acquire()
+                semaphore[0] -= 1
+                while semaphore[0] <= 0:
+                    pass
                 gt_promise = executor.submit(get_game_tensor,game)
                 gt_promise.add_done_callback(lambda cb: _write_callback(cb,files,file_indexes,semaphore))
         

39f6193198b3c26f33f3c61ceec0d9b568279fff
Nir Oren <jhudsy@gmail.com>
2025-01-09 07:56:10 +0000

    turned debug off

--- web/app.py
+++ web/app.py
@@ -30,4 +30,4 @@ if __name__ == '__main__':
         print("Model file not found")
         exit(1)
 
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=False,host='0.0.0.0',port=8000)

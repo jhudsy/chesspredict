08490ef766b8a8137716f976ca843f96c5db4cfb
Nir Oren <jhudsy@gmail.com>
2025-01-16 20:03:06 +0000

    changed strip to replace

--- web/app.py
+++ web/app.py
@@ -13,7 +13,7 @@ model = keras.models.load_model(model_file)
  
 @app.route('/')
 def index():
-    return render_template('index.html',model_file=model_file.strip('models/'))
+    return render_template('index.html',model_file=model_file.replace('models/',''))
 
 @app.route('/submit_game', methods=['POST'])
 def submit_game():
